5a5f29d771a2e306b9d5958d4479d0a195a29591
hakemi <hakemiayoub612@gmail.com>
2022-12-23 00:44:26 +0100

    Diagramme uploaded

--- src/main/java/project/presentation/controller/TableController.java
+++ src/main/java/project/presentation/controller/TableController.java
@@ -24,14 +24,14 @@ public class TableController implements Initializable {
     }
 
     @FXML
-    public void CreateTableRoute(ActionEvent event) throws IOException {
+    public void CreateTableRoute() throws IOException {
         System.out.println("Create Table button pressed!");
         Parent root = FXMLLoader.load(project.presentation.frame.Table.class.getResource("CreateTableComponent.fxml"));
         contentArea.getChildren().setAll(root);
     }
 
     @FXML
-    public void getAllTables(ActionEvent event) throws IOException {
+    public void getAllTables() throws IOException {
         System.out.println("Get All Tables button pressed!");
         Parent root = FXMLLoader.load(project.presentation.frame.Table.class.getResource("AllTablesComponent.fxml"));
         contentArea.getChildren().setAll(root);
