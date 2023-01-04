f99370be66bfc8b74b3dbeeedf690139f788495e
hakemi <hakemiayoub612@gmail.com>
2023-01-04 17:24:55 +0100

    Menu uploaded

---
+++ src/main/java/project/presentation/controller/menu/MenuController.java
@@ -0,0 +1,38 @@
+package project.presentation.controller.menu;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button allAccounts;
+
+    @FXML
+    private Button opinionsAdmin;
+
+    @FXML
+    private Button restaurantManage;
+
+    @FXML
+    private Button payments;
+
+    @FXML
+    private Button restaurants;
+
+    @FXML
+    private Button myOpinions;
+
+    @FXML
+    private Button myPayments;
+
+    @FXML
+    private Button myReservations;
+
+    @FXML
+    private Button notifCenter;
+
+    @FXML
+    private Button reservations;
+
+}
